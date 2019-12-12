92b87b687665c2ebbdac35f110a9886139abfc53
schuster <schuster@mpp.mpg.de>
2019-12-12 18:38:38 +0100

    added group_by_evtno function

--- src/geant4_hdf5.jl
+++ src/geant4_hdf5.jl
@@ -1,31 +1,25 @@
 # This file is a part of LegendDataTypes.jl, licensed under the MIT License (MIT).
 
-function read_geant4_hdf5 end
-export read_geant4_hdf5
-
-
+export Geant4HDF5Input
 struct Geant4HDF5Input <: AbstractLegendInput
     hdf5file::HDF5.HDF5File
 end
 
-export Geant4HDF5Input
+struct GEARS_HDF5Input <: AbstractLegendInput
+hdf5file::HDF5.HDF5File
+end
+
+struct G4SIMPLE_HDF5Input <: AbstractLegendInput
+hdf5file::HDF5.HDF5File
+end
 
 Base.open(filename::AbstractString, ::Type{Geant4HDF5Input}) =
     Geant4HDF5Input(HDF5.h5open(filename, "r"))
 
 Base.close(input::Geant4HDF5Input) = close(input.hdf5file)
 
-
 function Base.getindex(input::Geant4HDF5Input, ::Colon)
     read(input)
-end
-
-struct GEARS_HDF5Input <: AbstractLegendInput
-    hdf5file::HDF5.HDF5File
-end
-
-struct G4SIMPLE_HDF5Input <: AbstractLegendInput
-    hdf5file::HDF5.HDF5File
 end
 
 function _d_open_if_exists(g, d_name::String)
@@ -99,6 +93,7 @@ function Base.read(input::GEARS_HDF5Input)
     )
     return hits
 end
+
 function Base.read(input::G4SIMPLE_HDF5Input)
     function _get_indices_raw(g)
         len = filter(x->x!=0x00, _d_open_if_exists(g, "e/pages")[:])
@@ -123,6 +118,7 @@ function Base.read(input::G4SIMPLE_HDF5Input)
     indices = [i for i in 1:n_ind]
 
     evtno = try Int32.(_d_open_if_exists(g, "event/pages")[:][indices]) catch ; ones(Int32, n_ind) end
+    irep = try Int32.(_d_open_if_exists(g, "iRep/pages")[:][indices]) catch ; ones(Int32, n_ind) end
     detno = try Int32.(_d_open_if_exists(g, "volID/pages")[:][indices]) catch ; ones(Int32, n_ind) end
     thit = ( try Float32.(_d_open_if_exists(g, "t/pages")[:][indices]) catch ; zeros(Float32, n_ind) end ) .* u"s"
     edep = ( try Float32.(_d_open_if_exists(g, "Edep/pages")[:][indices]) catch ; zeros(Float32, n_ind) end ) .* u"keV"
@@ -136,26 +132,25 @@ function Base.read(input::G4SIMPLE_HDF5Input)
 
     ekin = ( try Float32.(_d_open_if_exists(g, "KE_data/pages")[:][indices]) catch ; zeros(Float32, n_ind) end ) .* u"keV"# kinetic Energy of track [keV]
     stp = try Int32.(_d_open_if_exists(g, "step/pages")[:][indices]) catch ; zeros(Int32, n_ind) end # step number
-    # l = ( try Float32.(_d_open_if_exists(g, "l_data/pages")[:][indices]) catch ; zeros(Float32, n_ind) end  ) .* u"mm"# step length
     mom = try Int32.(_d_open_if_exists(g, "parentID/pages")[:][indices]) catch ; zeros(Int32, n_ind) end # parent id
     trk = try Int32.(_d_open_if_exists(g, "trackID/pages")[:][indices]) catch ; zeros(Int32, n_ind) end # trk id
     pdg = try Int32.(_d_open_if_exists(g, "pid/pages")[:][indices]) catch ; zeros(Int32, n_ind) end # particle id
-    # pro = try Int32.(_d_open_if_exists(g, "pro_data/pages")[:][indices]) catch ; zeros(Int32, n_ind) end # process id
 
     hits = TypedTables.Table(
         evtno = evtno,
-        detno = detno,
+        detno = irep,
         thit = thit,
         edep = edep,
         pos = pos,
         ekin = ekin,
+        volID = detno,
         stp = stp,
-        # l = l,
         mom = mom,
         trk = trk,
         pdg = pdg,
-        # pro = pro,
     )
     return hits
-    println("So far, so good...")
 end
+
+export group_by_evtno
+group_by_evtno(table::TypedTables.Table) = TypedTables.Table(consgroupedview(table[sortperm(table.evtno)].evtno, Tables.columns(table)))
