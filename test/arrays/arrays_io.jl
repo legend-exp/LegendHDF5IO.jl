0db435439d956f819ae036c2d117c616ead7d9ef
Felix Hagemann <hagemann@mpp.mpg.de>
2023-08-02 17:12:05 +0200

    Add basic tests for array I/O

---
+++ test/arrays/arrays_io.jl
@@ -0,0 +1,26 @@
+using Unitful
+using LegendHDF5IO: readdata, writedata, LHDataStore
+using HDF5
+
+@testset "Arrays I/O" begin
+    
+    a = rand(typeof(1.0u"keV"), 40)
+    h5open("array_test.h5", "w") do h5f
+        writedata(h5f, "array_in_keV", a)
+        writedata(h5f, "array_no_units", ustrip.(a))
+    end
+
+    @testset "readdata" begin
+        h5open("array_test.h5", "r") do h5f
+            @test readdata(h5f, "array_in_keV") == a
+            @test readdata(h5f, "array_no_units") == ustrip.(a)
+        end
+    end
+
+    @testset "LHDataStore" begin
+        LHDataStore("array_test.h5") do h5f
+            @test h5f["array_in_keV"][:] == a
+            @test h5f["array_no_units"][:] == ustrip.(a)
+        end
+    end
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@
 using Test
 
 Test.@testset "Package LegendHDF5IO" begin
+    include("arrays/arrays_io.jl")
     include("ranges/range_to_namedtuple.jl")
     include("histograms/histogram_io.jl")
     include("test_wrappers.jl")
