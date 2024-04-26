8982e1b333e806395210dfcbd4572dcd85a6c1c2
Arthur Butorev <ga53hud@mpp.mpg.de>
2024-04-26 14:09:12 +0200

    Bug fix for IO of DataSelectors in extension

--- ext/LegendHDF5IOLegendDataManagementExt.jl
+++ ext/LegendHDF5IOLegendDataManagementExt.jl
@@ -9,31 +9,7 @@ end
 using LegendDataManagement: DataSelector
 using LegendHDF5IO
 
-LegendHDF5IO._datatype_dict["expsetup"] = ExpSetup
-LegendHDF5IO._datatype_dict["datatier"] = DataTier
-LegendHDF5IO._datatype_dict["datapartition"] = DataPartition
-LegendHDF5IO._datatype_dict["dataperiod"] = DataPeriod
-LegendHDF5IO._datatype_dict["datarun"] = DataRun
-LegendHDF5IO._datatype_dict["datacategory"] = DataCategory
-LegendHDF5IO._datatype_dict["timestamp"] = Timestamp
-LegendHDF5IO._datatype_dict["filekey"] = FileKey
-LegendHDF5IO._datatype_dict["channelid"] = ChannelId
-LegendHDF5IO._datatype_dict["detectorid"] = DetectorId
-
-
-
-dataselector_bytypes = Dict{Type, String}(
-    ExpSetup => "expsetup",
-    DataTier => "datatier",
-    DataPartition => "datapartition",
-    DataPeriod => "dataperiod",
-    DataRun => "datarun",
-    DataCategory => "datacategory",
-    Timestamp => "timestamp",
-    FileKey => "filekey",
-    ChannelId => "channelid",
-    DetectorId => "detectorid"
-)
+const dataselector_bytypes = Dict{Type, String}()
 
 LegendHDF5IO.datatype_to_string(::Type{<:T}) where {T <: DataSelector} = 
     dataselector_bytypes[T]
@@ -74,6 +50,30 @@ function LegendHDF5IO.LH5Array(ds::LegendHDF5IO.HDF5.Dataset, ::Type{<:AbstractA
    
     s = read(ds)
     T.(s)
+end
+
+function __init__()
+    LegendHDF5IO._datatype_dict["expsetup"] = ExpSetup
+    LegendHDF5IO._datatype_dict["datatier"] = DataTier
+    LegendHDF5IO._datatype_dict["datapartition"] = DataPartition
+    LegendHDF5IO._datatype_dict["dataperiod"] = DataPeriod
+    LegendHDF5IO._datatype_dict["datarun"] = DataRun
+    LegendHDF5IO._datatype_dict["datacategory"] = DataCategory
+    LegendHDF5IO._datatype_dict["timestamp"] = Timestamp
+    LegendHDF5IO._datatype_dict["filekey"] = FileKey
+    LegendHDF5IO._datatype_dict["channelid"] = ChannelId
+    LegendHDF5IO._datatype_dict["detectorid"] = DetectorId
+
+    dataselector_bytypes[ExpSetup] = "expsetup"
+    dataselector_bytypes[DataTier] = "datatier"
+    dataselector_bytypes[DataPartition] = "datapartition"
+    dataselector_bytypes[DataPeriod] = "dataperiod"
+    dataselector_bytypes[DataRun] = "datarun"
+    dataselector_bytypes[DataCategory] = "datacategory"
+    dataselector_bytypes[Timestamp] = "timestamp"
+    dataselector_bytypes[FileKey] = "filekey"
+    dataselector_bytypes[ChannelId] = "channelid"
+    dataselector_bytypes[DetectorId] = "detectorid"
 end
 
 end
