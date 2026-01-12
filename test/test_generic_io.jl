f95fb5afb31ffefba10942099796637e2294404c
Jonas Schlegel <jonas.schlegel@mpi-hd.mpg.de>
2026-01-12 12:39:24 +0100

    Add test for sorting

---
+++ test/test_generic_io.jl
@@ -0,0 +1,21 @@
+# This file is a part of LegendHDF5IO.jl, licensed under the MIT License (MIT).
+
+using Test
+using LegendHDF5IO
+
+using ArraysOfArrays
+using EncodedArrays
+using Measurements
+using RadiationDetectorSignals
+using StatsBase
+using TypedTables
+using Unitful
+
+@testset verbose=true "test generic IO" begin
+    @testset verbose=true "data types" begin
+        @test _sort_datatype_fields("table{values,dt,t0}") == "table{dt,t0,values}"
+        @test _sort_datatype_fields("struct{weights,isdensity,binning}") == "struct{binning,isdensity,weights}"
+        @test _sort_datatype_fields("real") == "real"
+        @test _sort_datatype_fields("array<1>{encoded_array<1>{real}}") == "array<1>{encoded_array<1>{real}}"
+    end
+end
